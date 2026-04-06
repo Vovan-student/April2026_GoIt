af9f682577caecc5814ec54ee7a0a17e5957e314
Volodymyr-old OS <boklah.vm@gmail.com>
2026-04-06 18:04:39 +0300

    change str5 delete str 2

--- main0604.py
+++ main0604.py
@@ -1,4 +1,5 @@
 print("Hello, dear friend!")
-print("Welcome to the world of Python programming.")
+
 print("Hello, dear friend!")
 print("Welcome to the world of Python programming.")
+print("Hello, world!")
