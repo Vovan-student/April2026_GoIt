475802a0e0d8740292ab014d5dccd521a680b806
Volodumyr Boklah <boklah.vm@gmail.com>
2026-04-10 23:27:08 +0300

    10.04  цикли

--- main0604.py
+++ main0604.py
@@ -1,4 +1,6 @@
-my_list=["table", "chair", "lamp", "sofa", "bed"]
-print(my_list[4])
-my_list[2]="wardrobe"
-print(my_list[2])
+sentence = "The quick brown fox jumps over the lazy dog"
+length = 0
+for i in sentence:
+   if i!=" ":
+       length = length + 1
+print("The length of the sentence is:", length)
