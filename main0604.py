6cd5352f5045ce615b8c2eb14c97c6cfd6c2d0a1
Volodymyr-old OS <boklah.vm@gmail.com>
2026-04-11 09:37:13 +0300

    11/04 add

--- main0604.py
+++ main0604.py
@@ -1,6 +1,11 @@
 sentence = "The quick brown fox jumps over the lazy dog"
 length = 0
+gaps = 0
 for i in sentence:
    if i!=" ":
        length = length + 1
+   else:
+       gaps = gaps + 1
+
 print("The length of the sentence is:", length)
+print("The number of gaps in the sentence is:", gaps)
