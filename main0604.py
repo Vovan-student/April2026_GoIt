91698dbda513e00ec7510560a35522c9d6f0c3c6
Volodumyr Boklah <boklah.vm@gmail.com>
2026-04-13 21:32:43 +0300

    range

--- main0604.py
+++ main0604.py
@@ -1,13 +1,16 @@
-sentence = "The quick brown fox jumps over the lazy dog"
-length = 0
-gaps = 0
+# sentence = "The quick brown fox jumps over the lazy dog"
+# length = 0
+# gaps = 0
 
-for i in sentence:
-   if i!=" ":
-       length = length + 1
-   else:
-       gaps = gaps + 1
+# for i in sentence:
+#    if i!=" ":
+#        length = length + 1
+#    else:
+#        gaps = gaps + 1
        
-print("The length of the sentence is:", length)
-print("The number of gaps in the sentence is:", gaps)
-print("The total number of characters in the sentence is:", length + gaps)
+# print("The length of the sentence is:", length)
+# print("The number of gaps in the sentence is:", gaps)
+# print("The total number of characters in the sentence is:", length + gaps)
+
+for i in range(6):
+    print(i)
