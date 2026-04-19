ddd634d3a3b2a77aec1cd704d1d008a9e396114f
Volodumyr Boklah <boklah.vm@gmail.com>
2026-04-19 22:22:38 +0300

    return

--- main0604.py
+++ main0604.py
@@ -1,39 +1,7 @@
+def add_numbers(a, b, c):
+    return a + b * c
 
-# N = 10
-# sum_squares = 0
-# i = 1
-# while i <= N:
-#     sum_squares = sum_squares + i**2
-#     i = i + 1
+result = add_numbers(5, 3, 8)
+print(result)  
 
-# print(f"The sum of the squares of numbers from 1 to {N} is {sum_squares}")
-
-# def say_hello(name):
-#     print("Привіт,", name)
-# say_hello("Світ")
-# say_hello("Masha")
-
-# def function():
-#     print("Hello world!")
-# function()  
-# def print_message(message):
-#     print(message)
-
-# print_message("Це повідомлення з функції.")
-
-def function(message):
-    print (message) 
-function("Hello world!")
-
-message = "Hello friend!"
-def function(message):    
-    print (message)
-function(message)
-
-message = "Hello world!"
-
-def function(message):
-    print(message)
-
-function(message)
     
