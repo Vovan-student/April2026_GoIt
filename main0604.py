67cccdc8036ca719a4296f7167d55075405baf7f
Volodumyr Boklah <boklah.vm@gmail.com>
2026-05-14 22:38:42 +0300

    list 140526

--- main0604.py
+++ main0604.py
@@ -1,24 +1,3 @@
-# # Введення (отримання даних)
-# ім_я = input("Введіть ваше ім'я: ")
-
-# # Перетворення (обробка даних)
-# вітання = f"Привіт, {ім_я}!"
-
-# # Виведення (виведення даних)
-# print(вітання)
-# age = 20
-# user_name = "Boris"
-# user_age = 30
-# ADULT_THR = 18
-name = "Oleg"
-s1 = "Hello"
-s2 = "world!"
-joinedstring = s1 + " " + s2
-joined_string = f"{s1} {s2}"
-# print(joined_string)
-# print(joinedstring)
-print("Привіт", end=" ")
-print(name)
-a = input("Рядок запрошення: ")
-# На екрані ви побачите: Рядок запрошення:
-print(a)
+my_list = [1, 2, 3, 4, 2, 2, 2, 5, 2, 2]
+count_2 = my_list.count(2)
+print(count_2)  # Виведе 4, оскільки число 2 зустрічається 4 рази
